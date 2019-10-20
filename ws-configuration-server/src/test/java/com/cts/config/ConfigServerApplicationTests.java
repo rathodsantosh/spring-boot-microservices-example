17dda2fca065c2e13817036637cc5d7613fe53dd
Santosh Rathod <santoshrathod.1876@gmail.com>
2019-10-20 19:06:40 +0530

    Fixed catalog service discovery issue and added zuul routes

--- ws-configuration-server/src/test/java/com/cts/config/ConfigServerApplicationTests.java
+++ ws-configuration-server/src/test/java/com/cts/config/ConfigServerApplicationTests.java
@@ -4,8 +4,8 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConfigServerApplicationTests {
+public class ConfigServerApplicationTests {
 
   @Test
-  void contextLoads() {}
+  public void contextLoads() {}
 }
